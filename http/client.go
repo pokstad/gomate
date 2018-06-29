d98c34586a63d41fc2e7c95f8cbd8bf1530630e1
Paul Okstad <paulokstad@pauls-mbp-2.lan>
2018-06-28 22:54:19 -0700

    http is not ready yet

--- http/client.go
+++
@@ -1,9 +0,0 @@
-package http
-
-// Client will dial to the UNIX socket used for gomate's server enable sending
-// commands. If server is not up, Client will start server process.
-type Client struct{}
-
-func Dial() (Client, error) {
-	return Client{}, nil
-}
